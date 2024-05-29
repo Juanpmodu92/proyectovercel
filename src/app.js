e5ef7004814bf84c6f9456b81f721c99a143c219
Juanpmodu92 <juanpmodu92@gmail.com>
2024-05-28 21:13:43 -0500

    Ajuste configuracion

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.json({ "message": "Welcome to my message API"})
+    res.json({ "message": "Welcome"})
 })
 
 
